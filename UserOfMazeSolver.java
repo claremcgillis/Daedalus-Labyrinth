0109a22e6ac4ca6c102d03aa3fc10f4667637567
Audrey-S00 <nsoo@stuy.edu>
2018-04-11 23:10:57 -0400

    Update UserOfMazeSolver.java

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,11 +1,11 @@
-/** 
+[0]/** 
 tests MazeSolver
 Command line requires the file path of the text file which contains the maze.
 */
 public class UserOfMazeSolver {
   public static void main(String[] commandLine) {
               
-      MazeSolver testingMaze = new MazeSolver(commandLine,
+      MazeSolver testingMaze = new MazeSolver(commandLine[0],
       																				Integer.parseInt( commandLine[1]), 
                                               Integer.parseInt( commandLine[2])
                                               );
