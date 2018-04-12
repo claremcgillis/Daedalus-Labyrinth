476feeb5c3b7ad316f7362068afe31c5e5a48d41
Audrey-S00 <nsoo@stuy.edu>
2018-04-11 23:09:16 -0400

    pasted UserOfMazeSolver code. not tested yet

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,2 +1,15 @@
+/** 
+tests MazeSolver
+Command line requires the file path of the text file which contains the maze.
+*/
 public class UserOfMazeSolver {
+  public static void main(String[] commandLine) {
+              
+      MazeSolver testingMaze = new MazeSolver(commandLine,
+      																				Integer.parseInt( commandLine[1]), 
+                                              Integer.parseInt( commandLine[2])
+                                              );
+      System.out.println( testingMaze + System.lineSeparator());
+      }
+                                              
 }
