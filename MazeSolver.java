2329c0e4177af8885ee8112a7809c945b39104a1
Audrey-S00 <nsoo@stuy.edu>
2018-04-12 22:03:10 -0400

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,35 +1,81 @@
 /**
+
 Pre condition: given a text file with a maze and placement of explorer
 
+
+
 Post condition: returns a boolean value of whether there is a solution 
+
 to the maze starting at a designated beginning, ending at the treasure
+
 without reaching any stepping stone more than once.
 
+
+
 */
+
 public class MazeSolver {
+
 	private boolean isMazeSolved;
+
 	public Maze inProgress;
+
 	
+
 	
+
 	public MazeSolver(String sourceFilename, int explorerRank, int explorerFile) throws java.io.FileNotFoundException{ 
+
   		 
+
   		inProgress = new Maze(sourceFilename, explorerRank, explorerFile);
-		System.out.println(isMazeSolved);
+
+		System.out.println("testing. created inProgress");
+
+		pathFinder();
+
+		System.out.println("Finished pathFinder. status of treasure: " + isMazeSolved);
+
 	}
+
+
 
 	public boolean getIsMazeSolved() {
+
 		return isMazeSolved;
-	}
-	
-	public String toString() {
-		return inProgress.toString();
+
 	}
 
-    public void pathFinder() {
-	if (inProgress.explorerIsOnA() == 0) { // Explorer on treasure
-	    isMazeSolved = true;
+	
+
+	public String toString() {
+
+		return inProgress.toString();
+
 	}
+
+
+
+    public void pathFinder() {
+
+	if (inProgress.explorerIsOnA() == 0) { // Explorer on treasure
+
+	    isMazeSolved = true;
+
+		System.out.println(isMazeSolved);
+
+	}
+
+	else{
+
+		System.out.println("Explorer not on treasure");
+
+	}
+
     }
+
     
+
+
 
 }
