00099806a8f2487b3c7c5df8e3d3b140ac2493c9
claremcgillis <34114897+claremcgillis@users.noreply.github.com>
2018-04-12 22:26:51 -0400

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,81 +1,51 @@
 /**
-
 Pre condition: given a text file with a maze and placement of explorer
 
-
-
 Post condition: returns a boolean value of whether there is a solution 
-
 to the maze starting at a designated beginning, ending at the treasure
-
 without reaching any stepping stone more than once.
 
-
-
 */
-
 public class MazeSolver {
-
 	private boolean isMazeSolved;
-
 	public Maze inProgress;
-
 	
-
 	
-
 	public MazeSolver(String sourceFilename, int explorerRank, int explorerFile) throws java.io.FileNotFoundException{ 
-
   		 
-
   		inProgress = new Maze(sourceFilename, explorerRank, explorerFile);
-
-		System.out.println("testing. created inProgress");
-
-		pathFinder();
-
-		System.out.println("Finished pathFinder. status of treasure: " + isMazeSolved);
-
 	}
-
-
 
 	public boolean getIsMazeSolved() {
-
 		return isMazeSolved;
-
 	}
-
 	
-
 	public String toString() {
-
 		return inProgress.toString();
-
 	}
 
-
-
-    public void pathFinder() {
-
-	if (inProgress.explorerIsOnA() == 0) { // Explorer on treasure
-
-	    isMazeSolved = true;
-
-		System.out.println(isMazeSolved);
-
-	}
-
-	else{
-
-		System.out.println("Explorer not on treasure");
-
-	}
-
+  public void pathFinder() {
+  	
+	if (inProgress.explorerIsOnA() == 0)  // Explorer on treasure
+	    {
+      isMazeSolved = true;
+      break;
+      }
+  if else (inProgress.explorerIsOnA() == 1) // Explorer in a wall {
+  		inProgress = lastSnap;
+  }
+  if else (inProgress.explorerIsOnA() == 2) // Explorer is on a stepping stone {
+  		Maze lastSnap = new Maze(inProgress);
+  		for (int i = 0; i < 4; i++) {
+      	inProgress.dropA(1); // makes the stepping stone a wall
+      	inProgress.go(Math.exp(2,i));
+        inProgress.pathFinder();
+      }
+  }
+  }
+      
+      
     }
-
     
-
-
 
 }
