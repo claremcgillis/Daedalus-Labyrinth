fb6931130f2e97112aaf0050b1739d04909d22e7
claremcgillis <34114897+claremcgillis@users.noreply.github.com>
2018-04-15 19:53:48 -0400

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -15,7 +15,7 @@ public class MazeSolver {
 	public MazeSolver(String sourceFilename, int explorerRank, int explorerFile) throws java.io.FileNotFoundException{ 
   		 
   		inProgress = new Maze(sourceFilename, explorerRank, explorerFile);
-		System.out.println(pathFinder(inProgress));
+		//System.out.println(pathFinder(inProgress));
 	}
 
 	public boolean getIsMazeSolved() {
